c96a3b075ce69d5f6f55fa0176a12f069f176eb2
oldchevy <nick.olszowy@gmail.com>
2016-07-27 16:02:31 -0700

    Completes functional-shared queue.
    -Note: You can't make private var's with this class style! Must add them to the instance

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,46 @@
 var Queue = function() {
+
+  var instance = {
+    count: 0,
+    storage: {}
+  };
+
+
+  _.extend(instance, queueMethods);
+
+  return instance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  size: function() {
+    return this.count;
+  },
+
+  enqueue: function(value) {
+    var count = this.count;
+    var storage = this.storage;
+    for (var i = count; count > 0; count--) {
+      storage[i] = storage[i - 1];
+    }
+
+    storage[0] = value;
+    this.count++;
+
+  },
+
+  dequeue: function() {
+
+    this.count = Math.max(0, this.count - 1);
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+
+    return result;
+
+  }
+
+};
 
 
