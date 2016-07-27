e38ce3f27b77512efe48376c26f60a6068523f7b
oldchevy <nick.olszowy@gmail.com>
2016-07-27 15:10:44 -0700

    Completes functional Queue class

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var index = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
+    var lowestIndex = Math.max(0, Object.keys(storage)[0]);
+    var result = storage[lowestIndex];
+    delete storage[lowestIndex];
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
