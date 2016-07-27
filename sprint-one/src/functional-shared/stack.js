9bfcadd169c1f2b03ac219b4ba9fd41584a6e421
oldchevy <nick.olszowy@gmail.com>
2016-07-27 16:28:57 -0700

    Completes functional-shared stack
    -Got rid of count var by using Object.keys(storage)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
+  var instance = {
+    storage: {}
+  };
+
+  _.extend(instance, stackMethods);
+
+  return instance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return Object.keys(this.storage).length;
+  },
+  push: function(value) {
+    this.storage[this.size()] = value;
+  },
+  pop: function() {
+    var result = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    
+    return result;
+  }
+};
 
 
