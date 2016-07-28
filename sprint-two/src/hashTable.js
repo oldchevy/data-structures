1dda07752a1d74a1389fe6b3fc6620b1ea3146cc
oldchevy <nick.olszowy@gmail.com>
2016-07-28 11:41:37 -0700

    Working on Hash, replaced 'xit' with 'it' in specs

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,35 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  // while (this._storage.get(index) ) {
+  //   this._limit *= 2;
+  //   var temp = this._storage;
+  //   this._storage = LimitedArray(this._limit);
+  //   temp.each(function(val, i) {
+  //     this._storage.set(i, val);
+  //   });
+  //   index = getIndexBelowMaxForKey(k, this._limit);
+  // }
+  
+  var value = { k: v};
+
+  this._storage.set(index, value);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(entry, i, arr) {
+    if (i === index) {
+      arr.splice(i, 1);
+    }
+  });
 };
 
 
