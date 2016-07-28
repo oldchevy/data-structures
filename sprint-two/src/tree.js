a3b5a4c9f4ac901e913a5c067a6071f270c4d339
oldchevy <nick.olszowy@gmail.com>
2016-07-27 21:10:20 -0700

    Completes tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,38 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
+
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+  this.children.push(Tree(value));
+  
+  //newTree.children = [];  // fix me
 };
 
 treeMethods.contains = function(target) {
+
+  var bool = false;
+
+  if (this.value === target) {
+    return true;
+  }
+
+  _.each(this.children, function(child) {
+
+    if (child.contains(target)) {
+      bool = true;
+    }
+  });
+
+  return bool;
 };
 
 
